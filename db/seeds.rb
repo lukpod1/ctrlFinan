28b4b79d3deafe416750efa1332615ba2b168693
Jaicke Santos <jaicke-santos@outlook.com>
2019-09-05 12:21:17 -0300

    Corrige seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'Criando usuário'
+User.create!(name: Faker::Name.name, email: 'admin@admin.com', password: 12345678, password_confirmation: 12345678 )
+puts 'Usuário criado.'
 
+puts 'Criando contas.'
 10.times do
   Account.create!(name: Faker::Lorem.word, balance: Faker::Number.decimal(l_digits: 2), user_id: User.first.id)
 end
+puts 'Contas criadas.'
